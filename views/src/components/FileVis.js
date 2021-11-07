7b3c46a9fd28617a18d4ae2fdcfbbefe143597b0
Mingcheng Xu <44307188+mcxxmc@users.noreply.github.com>
2021-11-07 21:45:04 +0800

    update 2021/11/7
    add front end renaming file
    
    todo:
    crud
    different users

--- views/src/components/FileVis.js
+++ views/src/components/FileVis.js
@@ -1,14 +1,84 @@
 import React from "react";
+import "./css/FileVis.css"
 
 class FileVis extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            filename: this.props.filename,
+            content: this.props.content,
+            show: true,
+            rename: false,
+            newName: this.props.filename.slice(0, -4),
+        }
+        this.handleChangeCtt = this.handleChangeCtt.bind(this)
+        this.handleChangeRename = this.handleChangeRename.bind(this)
+    }
+
+    // check if there is a change in the state (due to changes from the parent)
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.filename !== this.props.filename) {
+            this.setState({filename: this.props.filename,
+                content: this.props.content})
+        }
+    }
+
+    /**
+     * handles the change for modifying context
+     * @param event
+     */
+    handleChangeCtt(event) {
+        this.setState({content: event.target.value})
+    }
+
+    /**
+     * handles the change for renaming
+     * @param event
+     */
+    handleChangeRename(event) {
+        this.setState({newName: event.target.value})
+    }
+
     render() {
+        if (!this.state.show) {
+            return <button onClick={() => this.setState({show: true})}>Show File</button>
+        }
+
+        let rename;
+        if (this.state.rename) {
+            rename = (
+                <div>
+                    <textarea value={this.state.newName} onChange={this.handleChangeRename}
+                              className={"file-vis-textarea-rename"}/>
+                    <p className={"file-vis-p"}>.txt</p>
+                    <button onClick={() => this.rename()}>Confirm</button>
+                    <button onClick={() => this.setState({rename: false})}>Cancel</button>
+                </div>
+            )
+        }
+
         return (
             <div>
-                <h1>{this.props.fileName}</h1>
-                <p>{this.props.content}</p>
+                <button onClick={() => this.setState({show: false})}>Collapse</button>
+                <h1 className={"file-vis-h1"}>{this.state.filename}</h1>
+                <button onClick={() => this.setState({rename: true})}>Rename</button>
+                <button>Delete</button>
+                {rename}
+                <br/>
+                <textarea value={this.state.content} onChange={this.handleChangeCtt} className={"file-vis-textarea"}/>
+                <button onClick={() => this.commit()}>Commit</button>
             </div>
         )
+    }
+
+    commit() {
+        this.props.callbackModifyFile(this.state.content)
+    }
+
+    rename() {
+        this.props.callbackRenameFile(this.state.newName + ".txt")
+        this.setState({rename: false})
     }
 }
 
--- views/src/components/Hierachy.js
+++ views/src/components/Hierachy.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Dir from "./Dir"
 import File from "./File";
-import ReactDOM from "react-dom";
 import FileVis from "./FileVis";
 import {user2url} from "../constants/constants";
 
@@ -11,7 +10,10 @@ class Hierarchy extends React.Component {
         super(props);
         this.state = {
             organized: [],
-            user: this.props.userId
+            user: this.props.userId,
+            fileOnDisplay: -1,  // the index of the file that is now displayed. -1 means none.
+            filename: "",  // the filename of the file that is now displayed
+            content: "",  // the content of the file that is now displayed
         }
         this.cache = []
     }
@@ -34,7 +36,63 @@ class Hierarchy extends React.Component {
         })
             .then(response => response.json())
             .catch(error => console.error("Error: ", error))
-            .then(response => visFile(response))
+            .then(response => this.setState({fileOnDisplay: i,
+                filename: response["file_name"], content: response["content"]}))
+    }
+
+    /**
+     * The callback function for modifying file
+     * The file modified will be the current file on display
+     * @param {string} childData
+     */
+    callbackModifyFile = (childData) => {
+        if (this.state.fileOnDisplay === -1) {
+            console.error("modifyFile error: no file selected")
+            return
+        }
+        fetch(user2url["post"]["modifyFile"], {
+            body: JSON.stringify({
+                "user": this.state.user,
+                "fid": this.state.organized[this.state.fileOnDisplay]["id"],
+                "new_c": childData}),
+            method: 'POST'
+        }).catch(error => console.error("Error when modifying file: ", error))
+            .then(() => alert("success"))
+    }
+
+    /**
+     * The callback function for renaming a file.
+     * The file renamed will be the current file on display
+     * @param childData
+     */
+    callbackRenameFile = (childData) => {
+        if (this.state.fileOnDisplay === -1) {
+            console.error("renameFile error: no file selected")
+            return
+        }
+        let tmp = this.state.organized[this.state.fileOnDisplay];
+        if (tmp["dir"]) {
+            console.error(("renameFile error: not a file"))
+            return
+        }
+        let msg = "Are you sure to rename file " + tmp["name"] + " to " + childData + "?";
+        if (window.confirm(msg)) {
+            fetch(user2url["post"]["rename"], {
+                body: JSON.stringify({
+                    "user": this.state.user,
+                    "obj_id": tmp["id"],
+                    "dir": tmp["dir"],
+                    "new_name": childData
+                }),
+                method: 'POST'
+            }).catch(error => console.error("Error when renaming file", error))
+                .then(() => alert("success"))
+        }
+        tmp = this.state.organized;
+        tmp[this.state.fileOnDisplay]["name"] = childData;
+        this.setState({
+            organized: tmp, filename: childData
+        })
     }
 
     /**
@@ -53,9 +111,16 @@ class Hierarchy extends React.Component {
     }
 
     render() {
+        let visFile;
+        if (this.state.fileOnDisplay !== -1) {
+            visFile = <FileVis filename={this.state.filename} content={this.state.content}
+                               callbackModifyFile={this.callbackModifyFile}
+                               callbackRenameFile={this.callbackRenameFile}/>
+        }
         return (
             <div>
                 {this.state.organized.map((x, i) => this.display(x, i))}
+                {visFile}
             </div>
         )
     }
@@ -86,17 +151,6 @@ class Hierarchy extends React.Component {
             console.error(error);
         }
     }
-}
-
-/**
- * renders the file visualization
- * @param {JSON} response
- */
-function visFile(response) {
-    ReactDOM.render(
-        <FileVis fileName={response["file_name"]} content={response["content"]}/>,
-        document.getElementById("div_txt")
-    )
 }
 
 export default Hierarchy
